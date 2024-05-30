4a745640ebc6f3d241f43ae5e3161db29ed00b06
ThiagoRositi <110995578+ThiagoRositi@users.noreply.github.com>
2024-05-29 23:59:45 -0300

    Salon psql excersize with bash

---
+++ salon2.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+# initialization, define function
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
+
+MAIN_MENU () {
+  # Print 1st argument if given
+  if [[ $1 ]]; then
+    echo -e "\n$1"
+  fi
+
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id;")
+  echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+
+  SELECTOR
+}
+
+SELECTOR () {
+  
+  read SERVICE_ID_SELECTED
+
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]; then
+    MAIN_MENU "I could not find that service. What would you like today?"
+  else  
+    
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'" | sed -E 's/^ //g')
+
+    if [[ -z $SERVICE_NAME ]]; then
+      MAIN_MENU "I could not find that service. What would you like today?"
+    else :
+    fi
+  fi
+}
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
+MAIN_MENU
+echo -e "\nWhat's your phone number?"
+read CUSTOMER_PHONE
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'" | sed -E 's/^ +| +$//g')
+
+if [[ -z $CUSTOMER_NAME ]]; then
+  
+  echo -e "\nI don't have a record for that phone number, what's your name?"
+  read CUSTOMER_NAME
+
+  INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+fi
+echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+read SERVICE_TIME
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'" | sed -E 's/^ +| +$//g')
+INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME.\n"
